f2682d88210b35b842028e4ad1a6b540d3a34510
gamadv <lealdev.py@gmail.com>
2021-03-31 15:24:21 -0300

    ft: Create Newsletter Validation / Styling

--- src/components/Newsletter/index.tsx
+++ src/components/Newsletter/index.tsx
@@ -1,26 +1,114 @@
-import React from 'react';
-// import api from '../../services/api';
+import React, { useCallback, useState, ChangeEvent, FormEvent } from 'react';
+import * as Yup from 'yup';
+
+import api from '../../services/api';
+
+import getValidationErrors, { Errors } from '../../utils/helpers';
 
 import { Container } from './styles';
 
 const Newsletter: React.FC = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [sendStatus, setSendStatus] = useState(false);
+  const [sendError, setSendError] = useState<Errors>({} as Errors);
+
+  const newsletterSchema = Yup.object().shape({
+    email: Yup.string()
+      .email('Insira um e-mail válido.')
+      .required('Preencha com um e-mail válido'),
+    name: Yup.string().required('Preencha com seu nome completo'),
+  });
+
+  const postEmail = useCallback(async () => {
+    await api.post('newsletter', {
+      email: email,
+      name: name,
+    });
+  }, [name, email]);
+
+  const handleEmailInput = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      const { value } = event.target;
+      setEmail(value);
+    },
+    []
+  );
+  const handleNameInput = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      const { value } = event.target;
+
+      setName(value);
+    },
+    []
+  );
+  const handleBackNewsletter = useCallback(() => {
+    setSendStatus(false);
+  }, []);
+
+  const handleFormSubmit = useCallback(
+    async (event: FormEvent) => {
+      event.preventDefault();
+
+      try {
+        await newsletterSchema.validate({ name, email }, { abortEarly: false });
+        await postEmail();
+        setSendStatus(true);
+        setSendError({});
+
+        setEmail('');
+        setName('');
+      } catch (error) {
+        if (error instanceof Yup.ValidationError) {
+          const validationErrors = getValidationErrors(error);
+
+          setSendError(validationErrors);
+        }
+      }
+    },
+    [email, name, newsletterSchema, postEmail]
+  );
+
   return (
     <Container>
-      <form>
-        <fieldset>
-          <legend>Participe de nossas news com promoções e novidades!</legend>
-          <input
-            type="text"
-            name="contact-name"
-            placeholder="Digite seu nome"
-          />
-          <input
-            type="text"
-            name="contact-email"
-            placeholder="Digite seu email"
-          />
-          <button>Eu Quero!</button>
-        </fieldset>
+      <form onSubmit={handleFormSubmit}>
+        {sendStatus ? (
+          <div>
+            <p>
+              <strong>Seu e-mail foi cadastrado com sucesso!</strong>
+              <br />A partir de agora você receberá as novidades e ofertas
+              exclusivas.
+            </p>
+            <button onClick={handleBackNewsletter}>
+              Cadastrar novo e-mail
+            </button>
+          </div>
+        ) : (
+          <fieldset>
+            <legend>Participe de nossas news com promoções e novidades!</legend>
+            <div>
+              <input
+                type="text"
+                name="contact-name"
+                placeholder="Digite seu nome"
+                onChange={handleNameInput}
+                className={sendError.name ? 'error' : ''}
+              />
+              <span>{sendError.name}</span>
+            </div>
+            <div>
+              <input
+                type="text"
+                name="contact-email"
+                placeholder="Digite seu email"
+                className={sendError.email ? 'error' : ''}
+                onChange={handleEmailInput}
+              />
+              <span>{sendError.email}</span>
+            </div>
+            <button type="submit">Eu Quero!</button>
+          </fieldset>
+        )}
       </form>
     </Container>
   );
--- src/components/Newsletter/styles.ts
+++ src/components/Newsletter/styles.ts
@@ -9,6 +9,20 @@ export const Container = styled.section`
     margin: 0 auto;
     padding: 1rem 0;
 
+    div {
+      flex-direction: column;
+      p,
+      strong {
+        text-align: center;
+        font-style: normal;
+        letter-spacing: 0em;
+      }
+      p + button {
+        width: 37%;
+        margin: 1rem;
+      }
+    }
+    div,
     fieldset {
       display: flex;
       align-items: center;
@@ -20,17 +34,34 @@ export const Container = styled.section`
         color: #333333;
         font-weight: 700;
       }
-      input {
-        background: #ffffff;
-        border-radius: 5px;
-        margin: 0.7rem 0.2rem;
-        width: 25%;
-        padding: 1rem 1rem;
-      }
-      input::placeholder {
-        font-size: 12px;
-        color: 585858;
-        font-weight: 700;
+
+      div {
+        align-items: baseline;
+        flex-direction: column;
+        margin: 0;
+        input {
+          background: #ffffff;
+          border-radius: 5px;
+          margin: 0.7rem 0.2rem;
+          width: 280px;
+          padding: 1rem 1rem;
+
+          &.error {
+            border: thin solid #D7182A;
+            margin: 5px;
+            margin-bottom: -15px;
+          }
+        }
+        span {
+          color: #d7182a;
+          font-size: 12px;
+          padding-top: 1rem;
+        }
+        input::placeholder {
+          font-size: 12px;
+          color: 585858;
+          font-weight: 700;
+        }
       }
       button {
         margin: 0.2rem;
@@ -39,21 +70,29 @@ export const Container = styled.section`
         font-weight: 700;
         color: #ffffff;
         border-radius: 5px;
-        padding: 1rem 2rem;
+        padding: 1rem 2.5rem;
+        margin-bottom: 1rem;
+        
       }
 
       @media screen and (max-width: 768px) {
         flex-direction: column;
-
-        legend{
+        p + button {
+          margin-top: 1rem;
+          width: 90%;
+        }
+        p {
+          font-size: 14px;
+        }
+        legend {
           font-size: 20px;
           width: 95%;
           text-align: left;
         }
-        input,button {
+        input,
+        button {
           width: 95%;
         }
-
       }
     }
   }
